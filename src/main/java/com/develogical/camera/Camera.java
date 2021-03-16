8684e2e2ce0f4a64a64da595adf821fb8dd5a07a
Christian <christian.aumayr@d-fine.at>
2021-03-16 17:15:36 +0100

    xxxx

--- src/main/java/com/develogical/camera/Camera.java
+++ src/main/java/com/develogical/camera/Camera.java
@@ -2,16 +2,28 @@ package com.develogical.camera;
 
 public class Camera {
 
+    private MemoryCard memoryCard;
+    private Sensor sensor;
+
+    public Camera(Sensor sensor) {
+        this.sensor=sensor;
+    }
+
+    public Camera(Sensor sensor, MemoryCard memoryCard) {
+        this.sensor=sensor;
+        this.memoryCard=memoryCard;
+    }
+
     public void pressShutter() {
         // not implemented
     }
 
     public void powerOn() {
-        // not implemented
+        sensor.powerUp();
     }
 
     public void powerOff() {
-       // not implemented
+       sensor.powerDown();
     }
 }
 
--- src/test/java/com/develogical/camera/CameraTest.java
+++ src/test/java/com/develogical/camera/CameraTest.java
@@ -8,6 +8,31 @@ import static org.mockito.Mockito.*;
 public class CameraTest {
     @Test
     public void switchingTheCameraOnPowersUpTheSensor() {
-        // write your test here
+        Sensor sensor = mock(Sensor.class);
+        Camera underTest = new Camera(sensor);
+        underTest.powerOn();
+        verify(sensor).powerUp();
     }
+
+
+    @Test
+    public void switchingTheCameraOffPowersDownTheSensor() {
+        Sensor sensor = mock(Sensor.class);
+        Camera underTest = new Camera(sensor);
+
+        underTest.powerOff();
+
+        verify(sensor).powerDown();
+
+    }
+
+    @Test
+    public void pressingShutterCopiesData() {
+        Sensor sensor = mock(Sensor.class);
+        MemoryCard memoryCard = mock(MemoryCard.class);
+        Camera underTest = new Camera(sensor, memoryCard);
+
+
+    }
+
 }
